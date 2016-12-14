51decc94cc88b5a6993c05bca7d317026156055c
David Hudson <david@cognitivelogic.com>
2016-12-14 10:24:04 +0000

    Update day1 to use input file

--- day1/task_main.go
+++ day1/task_main.go
@@ -4,13 +4,24 @@ import (
 	"errors"
 	"fmt"
 	"math"
+	"os"
 	"strconv"
 	"strings"
+
+	"github.com/heedson/AoC2016/common"
 )
 
 // Run runs the task for the day. Same as a standalone 'main.go'.
 func Run() error {
-	input := "L1, R3, R1, L5, L2, L5, R4, L2, R2, R2, L2, R1, L5, R3, L4, L1, L2, R3, R5, L2, R5, L1, R2, L5, R4, R2, R2, L1, L1, R1, L3, L1, R1, L3, R5, R3, R3, L4, R4, L2, L4, R1, R1, L193, R2, L1, R54, R1, L1, R71, L4, R3, R191, R3, R2, L4, R3, R2, L2, L4, L5, R4, R1, L2, L2, L3, L2, L1, R4, R1, R5, R3, L5, R3, R4, L2, R3, L1, L3, L3, L5, L1, L3, L3, L1, R3, L3, L2, R1, L3, L1, R5, R4, R3, R2, R3, L1, L2, R4, L3, R1, L1, L1, R5, R2, R4, R5, L1, L1, R1, L2, L4, R3, L1, L3, R5, R4, R3, R3, L2, R2, L1, R4, R2, L3, L4, L2, R2, R2, L4, R3, R5, L2, R2, R4, R5, L2, L3, L2, R5, L4, L2, R3, L5, R2, L1, R1, R3, R3, L5, L2, L2, R5"
+	reader, err := os.Open("./day1/day1.txt")
+	if err != nil {
+		return err
+	}
+
+	input, err := common.LoadStringFromReader(reader)
+	if err != nil {
+		return err
+	}
 
 	commands := strings.Split(input, ", ")
 
